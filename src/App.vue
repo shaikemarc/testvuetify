cdba0a39091e7fbbc7371e3a1869212df52a7f1d
smarc <smarc@emergingmed.com>
2019-02-25 15:34:50 +0200

    vuetify 1.5.2 - same issue with checkbox

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <v-app>
     <v-content>
-   <v-autocomplete
-   clearable
-   :items="items"
-   item-text="display"
-   v-model="selected"
-   return-object
-   >
 
-   </v-autocomplete>
+   <v-checkbox
+     label="working"
+     value="v"
+     v-model="chkValue"
+     @change="chkChanged()"
+   ></v-checkbox>
+
+      <v-checkbox
+        label="not working"
+        value="v1"
+        :on-icon="'$vuetify.icons.checkbox_on'"
+        :off-icon="'$vuetify.icons.checkbox_off'"
+        v-model="chkValue1"
+        @change="chk1Changed()"
+      ></v-checkbox>
+
     </v-content>
   </v-app>
 </template>
@@ -17,20 +25,21 @@
 <script>
 
 
-
   export default {
     name: 'App',
 
+    methods: {
+      chkChanged() {
+       console.log(this.chkValue);
+      },
+      chk1Changed() {
+        console.log(this.chkValue1);
+      }
+    },
     data() {
       return {
-        selected: null,
-        items: [
-          {id: "1", display: "display1" },
-          {id: "2", display: "display2" },
-          {id: "3", display: "display3" },
-          {id: "4", display: "display4" },
-          {id: "5", display: "display5" },
-        ]
+        chkValue: null,
+        chkValue1: null
       }
     }
   }
--- src/mdIcons.js
+++ src/mdIcons.js
@@ -1,139 +1,24 @@
-import MenuDown from "vue-material-design-icons/MenuDown"
-import Close from "vue-material-design-icons/Close"
-import ExitToApp from "vue-material-design-icons/ExitToApp";
-import Home from "vue-material-design-icons/Home";
-import Apps from "vue-material-design-icons/Apps";
-import Check from "vue-material-design-icons/Check";
 
-import ArrowLeft from "vue-material-design-icons/ArrowLeft";
-import ArrowUp from "vue-material-design-icons/ArrowUp";
-import ArrowDown from "vue-material-design-icons/ArrowDown";
-
-import ChevronLeft from "vue-material-design-icons/ChevronLeft";
-import ChevronRight from "vue-material-design-icons/ChevronRight";
-import ChevronDown from "vue-material-design-icons/ChevronDown";
-import ChevronUp from "vue-material-design-icons/ChevronUp";
-import Plus from "vue-material-design-icons/Plus";
-import CLoseCircle from "vue-material-design-icons/CloseCircle";
-import Exclamation from "vue-material-design-icons/Exclamation";
-import HelpCircleOutline from "vue-material-design-icons/HelpCircleOutline";
-import BlockHelper from "vue-material-design-icons/BlockHelper";
+import CheckboxMarked from "vue-material-design-icons/CheckboxMarked";
+import CheckboxBlank from "vue-material-design-icons/CheckboxBlankOutline";
 
 const MdIcons = {
 
-  block: {
-    component: BlockHelper,
+  checkbox_on: {
+    component: CheckboxMarked,
     props: {
       title: ''
     }
   },
 
-  help_outline: {
-    component: HelpCircleOutline,
+  checkbox_off: {
+    component: CheckboxBlank,
     props: {
       title: ''
     }
   },
 
-  add: {
-    component: Plus,
-    props: {
-      title: ''
-    }
-  },
 
-  delete: {
-    component: CLoseCircle,
-    props: {
-      title: ''
-    }
-  },
-
-  warning: {
-    component: Exclamation,
-    props: {
-      title: ''
-    }
-  },
-
-  apps: {
-    component: Apps,
-    props: {
-      title: ''
-    }
-  },
-  check: {
-    component: Check,
-    props: {
-      title: ''
-    }
-  },
-  dropdown: {
-    component: MenuDown
-  },
-
-  // TODO, this cause issues at v-autocomplete :clearable
-  clear: {
-    component: Close
-  },
-
-  exit_to_app: {
-    component: ExitToApp,
-    props: {
-      title: ''
-    }
-  },
-  home: {
-    component: Home,
-    props: {
-      title: ''
-    }
-  },
-
-  arrow_back: {
-    component: ArrowLeft,
-    props: {
-      title: ''
-    }
-  },
-  arrow_upward: {
-    component: ArrowUp,
-    props: {
-      title: ''
-    }
-  },
-  arrow_downward: {
-    component: ArrowDown,
-    props: {
-      title: ''
-    }
-  },
-  expand: {
-    component: ChevronDown,
-    props: {
-      title: ''
-    }
-  },
-
-  shrink: {
-    component: ChevronUp,
-    props: {
-      title: ''
-    }
-  },
-
-  prev: {
-    component: ChevronLeft,
-    props: {
-      title: ''
-    }
-  },
-  next: {
-    component: ChevronRight,
-    props: {
-      title: ''
-    }
-  },
 };
 
 export {MdIcons};
